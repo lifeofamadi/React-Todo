b73e1d716ed31c02d1c9125f471ade1b49e6a83d
lifeofamadi <amadivictor3310@gmail.com>
2023-03-25 17:24:18 +0100

    A React Todo List

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./Components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Learn react" />
+      <Todo text="Master react" />
+      <Todo text="Earn in dollars" />
+    </div>
+  );
+}
+
+export default App;
